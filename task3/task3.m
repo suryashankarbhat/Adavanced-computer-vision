21de670da12b513eb33708c7057d68002811b44e
suryashankarbhat <92732366+suryashankarbhat@users.noreply.github.com>
2022-04-28 17:00:45 +0100

    Add files via upload

---
+++ task3/task3.m
@@ -0,0 +1,50 @@
+x=csvread('C:\Users\Computing\Downloads\x.csv');
+y=csvread('C:\Users\Computing\Downloads\y.csv');
+u=csvread('C:\Users\Computing\Downloads\a.csv');
+v=csvread('C:\Users\Computing\Downloads\b.csv');
+%plot(x, y, 'xb');
+%hold;
+%plot(u, v, '+r'); 
+%error = 0;
+
+nx = u - x;
+ny = v - y;
+%for i = 1:length(x)
+%    nx(i) = u(i) - x(i);
+%    ny(i)=v(i)-y(i);
+%    
+%end
+
+mean(nx)
+std(nx)
+mean(ny)
+std(ny)
+figure
+hist(nx,5)
+figure
+hist(ny,5)
+figure
+plot(x, y, 'xb');
+hold;
+plot(u, v, '+r');
+title('noise')
+
+%task2
+z=[u;v]
+[px, py] = kalmanTracking(z)
+plot(x, y, 'xb');
+hold;
+plot(px, py, '+r');
+
+
+
+
+plot(x, y, 'xb', px, py, '+r',u,v, 'og');
+legend({'x,y = real(x,y)','px,py = predicted(x,y)','u,v = noise(u,v)'},'Location','southeast')
+%hold;
+%plot(px, py, '+r');
+%hold;
+%plot(u,v);
+
+e = sqrt( ((x - px).^2)+((y - py).^2) )
+rms= rms(e)
